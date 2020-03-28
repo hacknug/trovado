84acc1001c0ec8404985856cb17488c3c2bf758a
Nestor Vera <nestorvera@me.com>
2020-03-28 20:59:09 +0100

    Add map to front-page

---
+++ src/components/HeroMap.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="bg-gray-50 lg:py-24 py-16 overflow-hidden">
+    <div class="sm:px-6 lg:px-8 lg:max-w-screen-xl relative max-w-xl px-4 mx-auto">
+
+      <svg class="lg:block left-full -translate-y-1/4 absolute hidden transform -translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784">
+        <defs>
+          <pattern id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+          </pattern>
+        </defs>
+        <rect width="404" height="784" fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
+      </svg>
+
+      <div class="relative">
+        <h3 class="sm:text-4xl sm:leading-10 text-3xl font-extrabold leading-8 tracking-tight text-center text-gray-900">
+          <ClientOnly>
+            A <vue-typer :text="['better', 'safer']" initialAction="erasing" /> way to shop during quarantine
+          </ClientOnly>
+        </h3>
+        <p class="max-w-3xl mx-auto mt-4 text-xl leading-7 text-center text-gray-500">
+          Queremos reducir el número de gente que baja al supermercado, poniéndose en riesgo, informándote de la disponibilidad de los productos más esenciales.
+        </p>
+      </div>
+
+      <div class="sm:rounded-lg relative mt-12 overflow-hidden bg-white shadow">
+        <div class="h-96 sm:p-6 px-4 py-5">
+          <mapbox-map
+            style="position: absolute;"
+            class="sm:rounded-lg absolute inset-0 w-full h-full"
+            access-token="pk.eyJ1IjoiaGFja251ZyIsImEiOiJjazhjMDN2Mm4waDN6M2VtamV3ZmdnMjB4In0.SQvCWv7t6pKfk_HOK_sZQg"
+            map-style="mapbox://styles/mapbox/streets-v11"
+            :center="center"
+            :trackUserLocation="true"
+            @mb-created="handleInstance"
+          />
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+export default {
+  name: 'HeroMap',
+  components: {
+    MapboxMap,
+    MapboxMarker,
+    VueTyper: () =>
+      import ('vue-typer')
+      .then(m => m.VueTyper)
+      .catch(),
+  },
+  data () {
+    return {
+      map: null,
+      center: [0, 0]
+    }
+  },
+  methods: {
+    handleInstance (mapboxInstance) {
+      this.map = mapboxInstance
+    },
+    flyTo () {
+      this.map.flyTo({ center: this.center })
+    },
+
+    geolocateUser () {
+      if (process.isClient) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          }
+        })
+      }
+    }
+  },
+  watch: {
+    center: 'flyTo',
+  },
+  async mounted () {
+    this.geolocateUser()
+  },
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,35 +1,8 @@
 <template>
   <Layout>
     <template slot="bleed">
-
-      <div class="bg-gray-50 lg:py-24 py-16 overflow-hidden">
-        <div class="sm:px-6 lg:px-8 lg:max-w-screen-xl relative max-w-xl px-4 mx-auto">
-
-          <svg class="lg:block left-full -translate-y-1/4 absolute hidden transform -translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784">
-            <defs>
-              <pattern id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width="404" height="784" fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
-          </svg>
-
-          <div class="relative">
-            <h3 class="sm:text-4xl sm:leading-10 text-3xl font-extrabold leading-8 tracking-tight text-center text-gray-900">
-              <ClientOnly>
-                A <vue-typer :text="['better', 'safer']" initialAction="erasing" /> way to shop during quarantine
-              </ClientOnly>
-            </h3>
-            <p class="max-w-3xl mx-auto mt-4 text-xl leading-7 text-center text-gray-500">
-              Queremos reducir el número de gente que baja al supermercado, poniéndose en riesgo, informándote de la disponibilidad de los productos más esenciales.
-            </p>
-          </div>
-
-        </div>
-      </div>
-
+      <HeroMap />
       <ShopList :shops="$page.shops.edges" />
-
     </template>
   </Layout>
 </template>
@@ -64,16 +37,11 @@
 </page-query>
 
 <script>
+import HeroMap from '~/components/HeroMap'
 import ShopList from '~/components/ShopList'
 
 export default {
-  components: {
-    ShopList,
-    VueTyper: () =>
-      import ('vue-typer')
-      .then(m => m.VueTyper)
-      .catch(),
-  },
+  components: { HeroMap, ShopList },
   metaInfo: {
     title: 'Home'
   },
