5c3d5d98e7294ee543bc1ad73e72efae8bd3019c
Nestor Vera <nestorvera@me.com>
2020-03-29 18:14:38 +0200

    Fix shop list layout missalignment

--- src/components/ShopList.vue
+++ src/components/ShopList.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="bg-gray-50 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 relative px-4 pt-16 pb-20">
+  <div class="bg-gray-50 lg:pt-24 lg:pb-28 relative pt-16 pb-20">
 
     <div class="absolute inset-0">
       <div class="h-1/3 sm:h-2/3 bg-white"></div>
     </div>
 
-    <div class="max-w-7xl relative mx-auto">
+    <div class="sm:px-6 lg:px-8 lg:max-w-screen-xl relative max-w-xl px-4 mx-auto">
 
       <div class="text-center">
         <h2 class="sm:text-4xl sm:leading-10 text-3xl font-extrabold leading-9 tracking-tight text-gray-900">Supermarkets with more stock</h2>
