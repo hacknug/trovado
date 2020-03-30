afa7be29a445ea8ee4b8e983d82ca33e2f4dfa3c
Nestor Vera <nestorvera@me.com>
2020-03-30 17:48:31 +0200

    Add image to HeroSearch component instead

--- src/components/HeroSearch.vue
+++ src/components/HeroSearch.vue
@@ -1,19 +1,31 @@
 <template>
-  <div class="relative overflow-hidden" style="height: 50vh;">
+  <div class="relative flex overflow-hidden" style="min-height: 50vh;">
     <div class="lg:block lg:absolute lg:inset-0 hidden">
-      <svg class="left-1/2 absolute top-0 transform translate-x-64 -translate-y-8" width="640" height="784" fill="none" viewBox="0 0 640 784">
-        <defs>
-          <pattern id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-          </pattern>
-        </defs>
-        <rect y="72" width="640" height="640" class="text-gray-50" fill="currentColor" />
-        <rect x="118" width="404" height="784" fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)" />
-      </svg>
+      <span class="left-1/2 absolute top-0 transform translate-x-64 -translate-y-8">
+        <svg class="" width="640" height="784" fill="none" viewBox="0 0 640 784">
+          <defs>
+            <pattern id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect y="72" width="640" height="640" class="text-gray-50" fill="currentColor" />
+          <rect x="118" width="404" height="784" fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)" />
+        </svg>
+        <figure class="-translate-y-1/8 -translate-x-1/6 absolute top-0 transform">
+          <g-image class="w-10/12" src="~/assets/img/cart.png" alt="Shopping cartio" />
+        </figure>
+      </span>
     </div>
 
-    <BaseContainer class="md:py-16 lg:py-20 w-full h-full py-12">
-      <div class="flex flex-col justify-center h-full">
+    <BaseContainer class="md:py-16 lg:py-20 w-full h-full py-12 my-auto">
+
+      <div class="lg:hidden z-0 mb-8">
+          <figure class="translate-x-1/4 translate-y-1/12 transform">
+            <g-image class="h-72 w-auto -mt-20" src="~/assets/img/cart.png" alt="Shopping cartio" />
+          </figure>
+      </div>
+
+      <div class="relative z-10 flex flex-col justify-center h-full">
         <ClientOnly>
           <h2 class="sm:text-4xl sm:leading-10 lg:w-1/2 flex flex-col text-3xl font-extrabold leading-9 tracking-tight">
             <span class="text-gray-900">Need <vue-typer :text="items" :shuffle="true" initialAction="erasing" />?</span>
