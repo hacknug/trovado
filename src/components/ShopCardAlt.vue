68da7abc8a297244f3b22c7a3fdd3678b5ece99d
Nestor Vera <nestorvera@me.com>
2020-04-04 12:04:59 +0200

    Finish mocking shop stock levels

--- src/components/ShopCardAlt.vue
+++ src/components/ShopCardAlt.vue
@@ -21,9 +21,10 @@
               <dd class="font-medium text-green-500">
                 <button
                   @click="open = !open"
-                  class="owl:ml-0.5 inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium leading-5 bg-green-100 text-green-800 focus:outline-none focus:shadow-outline-blue"
+                  class="owl:ml-0.5 inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium leading-5 focus:outline-none focus:shadow-outline-blue"
+                  :class="classNames[averageLabels[average]]"
                   aria-label="See Details"
-                  ><span>High</span>
+                  ><span>{{ averageLabels[average] }}</span>
                   <ChevronDownIcon class="w-5 h-5" />
                 </button>
               </dd>
@@ -46,14 +47,14 @@
 
       <div class="mr-auto">
         <dl class="grid grid-cols-2 row-gap-2 col-gap-12">
-          <div class="owl:ml-2 flex justify-between" v-for="([label, className], index) in stock" :key="index">
-            <dt class="owl:ml-1 flex items-center text-gray-500">{{ items[index] }}:</dt>
-            <dd :class="className" class="font-medium">{{ label }}</dd>
+          <div class="owl:ml-2 flex justify-between" v-for="({ item, stock }, index) in stock" :key="index">
+            <dt class="owl:ml-1 flex items-center text-gray-500">{{ item }}:</dt>
+            <dd :class="classNames[stock]" class="font-medium">{{ stock }}</dd>
           </div>
         </dl>
         <p class="mt-6 text-gray-400">
           <small>Updated yesterday by
-            <a href="#" class="px-1.5 py-1 text-gray-500 bg-gray-100 hover:text-gray-600 focus:bg-text-600 rounded">
+            <a href="#" class="px-1.5 py-1 text-gray-500 bg-blue-50 hover:text-gray-600 focus:bg-text-600 rounded">
               <code class="font-mono">concerned_citizen</code>
             </a>
           </small>
@@ -65,7 +66,7 @@
         <span class="sm:w-auto flex w-full rounded-md shadow-sm opacity-50 cursor-not-allowed">
           <button disabled @click="open = false; setTimeout(() => open = true, 1000)" type="button" class="group hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline sm:text-sm sm:leading-5 relative inline-flex justify-center w-full px-4 py-2 text-base font-medium leading-6 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm pointer-events-none">
             Update Stock
-            <span v-if="icon" class="absolute inset-y-0 left-0 flex items-center pl-2 text-gray-400">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-2 text-gray-400">
               <svg class="w-5 h-5 mr-2 stroke-current" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M12 6V10" />
                 <path d="M12 14H12.01" />
@@ -106,15 +107,17 @@ export default {
   data () {
     return {
       open: false,
-      icon: true,
-      items: ['Toilet Paper', 'Pasta', 'Yeast', 'Bread', 'Rice', 'Flour', 'Hand Sanitiser', 'Cleaning Products'],
-      recipe: [
-        ['+30 qty', 'text-green-400'],
-        ['10-30 qty', 'text-yellow-400'],
-        ['1-10 qty', 'text-red-400'],
-        ['sold out', 'text-gray-400'],
-        ['no info', 'text-black-400'],
-      ],
+      stock: [],
+      average: null,
+      sum: null,
+
+      recipe: {
+        stocks: ['no info', 'sold out', '1-10 qty', '10-30 qty', '+30 qty'],
+        items: [
+          'Toilet Paper', 'Hand Sanitiser', 'Fruit', 'Vegetables',
+          'Rice', 'Pasta', 'Bread', 'Flour', 'Yeast', 'Cleaning Products',
+        ],
+      },
     }
   },
   computed: {
@@ -122,6 +125,25 @@ export default {
 
     isFavourite () {
       return this.userData && this.userData.favourites && this.userData.favourites[this.shop.id]
+    },
+
+    averageLabels () {
+      return [ 'Unknown', 'None', 'Low', 'Medium', 'High' ]
+    },
+    classNames () {
+      return {
+        'Unknown': 'text-black-800 bg-black-100',
+        'None': 'text-gray-800 bg-gray-100',
+        'Low': 'text-red-800 bg-red-100',
+        'Medium': 'text-yellow-800 bg-yellow-100',
+        'High': 'text-green-800 bg-green-100',
+
+        'no info': 'text-black-400',
+        'sold out': 'text-gray-400',
+        '1-10 qty': 'text-red-400',
+        '10-30 qty': 'text-yellow-400',
+        '+30 qty': 'text-green-400',
+      }
     },
   },
   methods: {
@@ -142,9 +164,16 @@ export default {
     },
   },
   mounted () {
-    this.stock = this.items.map(() => {
-      return this.recipe[Math.floor(Math.random() * this.recipe.length)]
+    const random = (max) => Math.floor(Math.random() * max)
+
+    this.stock = this.recipe.items.map((item) => {
+      return { item, stock: this.recipe.stocks[random(this.recipe.stocks.length)] }
     })
+    this.sum = this.stock.reduce((accumulator, { stock }) => {
+      return accumulator + this.recipe.stocks.indexOf(stock)
+    }, 0)
+
+    this.average = Math.round((this.sum + this.stock.length) / this.stock.length)
   }
 }
 </script>
