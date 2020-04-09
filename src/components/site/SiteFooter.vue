03cdced538743c63f6e1292dfb661614a3ef0509
Nestor Vera <nestorvera@me.com>
2020-04-09 05:14:17 +0200

    Fix shadow overlapping footer

--- src/components/site/SiteFooter.vue
+++ src/components/site/SiteFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative bg-white">
+  <div class="relative z-10 bg-white">
     <div class="sm:px-6 md:flex md:items-center md:justify-between lg:px-8 max-w-screen-xl px-4 py-12 mx-auto">
 
       <div class="md:order-2 owl:ml-6 flex justify-center">
