554edca6b59b4b517b3d1461415d95e65d13f2a4
Nestor Vera <nestorvera@me.com>
2020-05-09 00:10:48 +0200

    Force users to signUp before saving a timeslot

--- src/components/ShopCard.vue
+++ src/components/ShopCard.vue
@@ -38,7 +38,7 @@
             </div>
           </dl>
 
-          <BaseButton @click.native.stop="$emit('bookTime', shop)" variant="secondary" size="sm">
+          <BaseButton @click.native.stop="!user ? SET_AUTH_INTENT('Sign Up') : $emit('bookTime', shop)" variant="secondary" size="sm">
             <template slot="icon"><CalendarIcon class="w-5 h-5 text-blue-600 stroke-current" /></template> Save your Timeslot
           </BaseButton>
 
@@ -99,7 +99,7 @@ const { db } = process.isClient ? require('~/firebase') : import('~/firebase')
 const places = db && db.collection('places')
 
 export default {
-  name: 'ShopCardMapbox',
+  name: 'ShopCard',
   components: {
     BaseButton,
     BarChart2Icon,
