a99015254f8ebbeb618b4b61e6240afe7c0aaf0a
Nestor Vera <nestorvera@me.com>
2020-05-08 23:31:23 +0200

    Fix default size for tabDays

--- src/components/ShopBookTime.vue
+++ src/components/ShopBookTime.vue
@@ -188,7 +188,7 @@ export default {
   & .tab__pagination > * + * { @apply mt-2 !important; }
 
   & .tab__days { @apply flex flex-col !important; }
-  & .tab__day { @apply flex flex-row border-b-0 !important; }
+  & .tab__day { @apply flex flex-none flex-row border-b-0 !important; }
   & .tab__day + .tab__day { @apply mt-6 !important; }
   & .tab__day > * + * { @apply ml-6 !important; }
   & .tab__days__day-display { @apply w-1/4 text-left !important; }
